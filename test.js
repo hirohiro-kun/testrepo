610a3b62fde9a32ca74f60cd41a82675f00716c4
hirohiro-kun <hkude.57125@gmail.com>
2022-02-23 09:58:01 +0900

    test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const request = require('sync-request');
-const url = "https://raw.githubusercontent.com/hirohiro-kun/testrepo/master/README.md"
+const url = "https://raw.githubusercontent.com/hirohiro-kun/testrepo/master/README.md";
 const response = request("GET", url);
 const bucketName = 'hk5gfree';
 const AWS = require('aws-sdk');
@@ -7,26 +7,25 @@ const AWS = require('aws-sdk');
 exports.handler =  async function(event, context) {
     //console.log(response.getBody().toString());
     function upload_to_s3() {
-        const s3 = new AWS.S3()
-        fileName = "README.md"
-        fileData = response.getBody().toString()
+        const s3 = new AWS.S3();
+        const fileName = "README.md";
+        const fileData = response.getBody().toString();
         const params = {
             Bucket: bucketName,
             Key: fileName,
             Body: fileData
-        }
+        };
         s3.upload(params, function(err, data) {
             if (err) {
-                console.log(err)
-                return
+                console.log(err);
+                return;
             }
             else {
-                console.log(data)
+                console.log(data);
             }
-        })
+        });
     }
 
-    upload_to_s3()
-}
+    upload_to_s3();
+};
   
-
