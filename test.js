876b0e6ff44ba048dd675e4ad1a64ba2f45e62a0
hirohiro-kun <hkude.57125@gmail.com>
2022-02-23 10:21:41 +0900

    test

---
+++ test.js
@@ -0,0 +1,30 @@
+const request = require('sync-request');
+const url = "https://raw.githubusercontent.com/hirohiro-kun/testrepo/master/README.md";
+const response = request("GET", url);
+const bucketName = 'hk5gfree';
+const AWS = require('aws-sdk');
+
+exports.handler = function(event, context, callback) {
+    //console.log(response.getBody().toString());
+    function upload_to_s3() {
+        const s3 = new AWS.S3();
+        const fileName = "README.md";
+        const fileData = response.getBody().toString();
+        const params = {
+            Bucket: bucketName,
+            Key: fileName,
+            Body: fileData
+        };
+        s3.upload(params, function(err, data) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            else {
+                console.log(data);
+            }
+        });
+    }
+    upload_to_s3();
+    return context.logStreamName;
+};
